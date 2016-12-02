e0d45d9ff89c36226fefdde220debe4572e6be9a
Ranie <wlfks369@gmail.com>
2016-12-02 18:53:42 +0900

    is it ok?
    please no conflict...

--- src/myPackage/TTC_Client.java
+++ src/myPackage/TTC_Client.java
@@ -20,20 +20,25 @@ public class TTC_Client {
 	static PrintWriter out;
 	JFrame frame = new JFrame("Time To Catch!");
 	JTextField textField = new JTextField(40);
-	JTextArea messageArea = new JTextArea(8, 40);	
+	JTextArea messageArea = new JTextArea(8, 40);
 
 	public TTC_Client() {
 
 		// Layout GUI - 이거 GUI 다 되면 고쳐야 함!
-		new StartRoom();
+		JFrame getFrame = new JFrame("Time To Catch");
+		getFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getFrame.setLayout(null);
+		getFrame.setBounds(200, 0, 1052, 764);
+		getFrame.add(new StartRoom());
+		getFrame.setVisible(true);
 
 		// Add Listeners
-//		textField.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				out.println(textField.getText());
-//				textField.setText("");
-//			}
-//		});
+		// textField.addActionListener(new ActionListener() {
+		// public void actionPerformed(ActionEvent e) {
+		// out.println(textField.getText());
+		// textField.setText("");
+		// }
+		// });
 	}
 
 	private String getName() {
@@ -47,7 +52,7 @@ public class TTC_Client {
 	private void run() throws IOException {
 
 		// Make connection and initialize streams
-		String serverAddress = "192.168.0.20";// Set server IP address
+		String serverAddress = "192.168.0.3";// Set server IP address
 		Socket socket = new Socket(serverAddress, 9001);
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		out = new PrintWriter(socket.getOutputStream(), true);
@@ -57,11 +62,9 @@ public class TTC_Client {
 			int c = 1;
 			line = in.readLine();
 			System.out.println(line);
-			if (line.startsWith("SUBMITNAME") && c == 1)
-			{
+			if (line.startsWith("SUBMITNAME") && c == 1) {
 				out.println(getName());
-			}
-			else if (line.startsWith("NAMEACCEPTED")) {
+			} else if (line.startsWith("NAMEACCEPTED")) {
 				textField.setEditable(true);
 				break;
 			} else
@@ -69,23 +72,22 @@ public class TTC_Client {
 			c++;
 			frame.setVisible(false);
 		} // set user's name!
-		
+
 		// game Start!
 		line = in.readLine();
-		if (line.startsWith(""))
+		if (line.startsWith("GAMESTART"))
 			gameStart();
 
 		// game ends!
 		socket.close();
 	}
 
-	public static void sendMode(String m)
-	{
-		out.println("MODE "+m);
+	public static void sendMode(String m) {
+		out.println("MODE " + m);
 	}
-	
+
 	private void gameStart() throws IOException {
-		
+
 	}
 
 	public static void main(String[] args) throws Exception {
