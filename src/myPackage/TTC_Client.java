aef1f1b45d72fe7b16842fd4850ca87076e54bd2
Ranie <wlfks369@gmail.com>
2016-12-07 22:39:45 +0900

    changed client
    ....

--- src/myPackage/TTC_Client.java
+++ src/myPackage/TTC_Client.java
@@ -1,32 +1,102 @@
 package myPackage;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import myPackage.StartPanel.ModeSelect;
 
 public class TTC_Client {
 
 	static BufferedReader in;
 	static PrintWriter out;
 	int pNum;
+	static int answer;
+	static String name;
 	JFrame frame = new JFrame("Info");
 	static JFrame mainFrame;
+	static JFrame wait;
+	JPanel panel;
 	JTextField textField = new JTextField(40);
 	JTextArea messageArea = new JTextArea(8, 40);
 
 	public TTC_Client() {
 		mainFrame = new JFrame();
+		panel = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(new ImageIcon("Login3.png").getImage(), 0, 0, getWidth(), getHeight(), this);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+
+		};
+
+		JButton Teambtn = new JButton(new ImageIcon("teammatch.png"));
+		JButton Singlebtn = new JButton(new ImageIcon("singlematch.png"));
+		Teambtn.setText("TEAM");
+		Singlebtn.setText("SINGLE");
+
+		Teambtn.setBorder(new EmptyBorder(10, 10, 10, 10)); // 테두리 투명화
+		Singlebtn.setBorder(new EmptyBorder(10, 10, 10, 10));
+		Teambtn.setBackground(new Color(9, 21, 52));
+		Singlebtn.setBackground(new Color(9, 21, 52));
+		mainFrame.add(Singlebtn);
+		mainFrame.add(Teambtn);
+		Singlebtn.setBounds(690, 350, 310, 40);
+		Teambtn.setBounds(718, 410, 280, 40);
+
+		Singlebtn.setBorderPainted(false);
+		Singlebtn.setFocusPainted(false);
+		Singlebtn.setContentAreaFilled(true);
+		Singlebtn.setOpaque(false);
+		Teambtn.setBorderPainted(false);
+		Teambtn.setFocusPainted(false);
+		Teambtn.setContentAreaFilled(true);
+		Teambtn.setOpaque(false);
+
+		Singlebtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// here do what you gotta do when the button is clicked
+				JButton b = (JButton) e.getSource();
+				TTC_Client.sendMode(b.getText());
+			}
+
+		});
+
+		Teambtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// here do what you gotta do when the button is clicked
+				JButton b = (JButton) e.getSource();
+				TTC_Client.sendMode(b.getText());
+			}
+
+		});
+
 		mainFrame.setTitle("Time to Catch!");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.add(new StartPanel());
+		panel.setVisible(true);
+		mainFrame.add(panel);
 		mainFrame.setResizable(false);
 		mainFrame.setSize(1052, 764);
 		mainFrame.setVisible(true);
@@ -51,21 +121,71 @@ public class TTC_Client {
 		int c = 1;
 
 		while (true) {
-			
+
 			line = in.readLine();
 			System.out.println(line);
 			if (line.startsWith("SUBMITNAME") && c == 1) {
-				out.println(getName());
+				name = getName();
+				out.println(name);
 			} else if (line.startsWith("NAMEACCEPTED")) {
 				break;
-			} else
-				out.println(getName("Enter another name:"));
+			} else {
+				name = getName("Enter another name:");
+				out.println(name);
+			}
 			c++;
 
 		}
-	
+
+		while (true) {
+
+			line = in.readLine();
+			System.out.println(line);
+			if (line.startsWith("WAIT")) {
+				wait = new JFrame();
+				JPanel p = new JPanel(){
+					public void paintComponent(Graphics g) {
+						g.drawImage(new ImageIcon("waiting.png").getImage(), 0, 0, getWidth(), getHeight(), this);
+						setOpaque(false);
+						super.paintComponent(g);
+					}
+				};
+						
+				wait.add(p);
+				wait.setSize(593,241);
+				wait.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				wait.setUndecorated(true);
+				wait.setVisible(true);
+				
+				//start wait!
+			}else if (line.startsWith("GAMESTART")) {
+				//stop wait!
+				wait.setVisible(false);
+				wait.dispose();
+			} else if (line.startsWith("TEAM")) {
+				line = line.substring(5, line.length());
+				//change gui!
+//				if (line.equalsIgnoreCase("infectee"))
+//					con.add(new GameRoomPanel("TTC_Gui_infectee.png"));
+//				else
+//					con.add(new GameRoomPanel("TTC_Gui_Noninfectee.png"));
+			} else if (line.startsWith("PROBLEM")) {
+				line = line.substring(8, line.length());
+				//print in frame!
+			} else if (line.startsWith("CHOICE")) {
+				line = line.substring(8, line.length());
+				//print in frame!
+			} else if (line.startsWith("ANSWER")) {
+				answer = Integer.parseInt(line.substring(7, line.length()));
+				//check and send result to server!
+			} else if (line.startsWith("NOTICE")) {
+				line = line.substring(7, line.length());
+				//print in frame!
+			}
+		}
+
 	}
-	
+
 	public static BufferedReader getIn() {
 		return in;
 	}
@@ -73,79 +193,13 @@ public class TTC_Client {
 	public static void sendMode(String m) {
 		out.println("MODE " + m);
 	}
-	public static void getReady(){
-		
-		String line;
-		try {
-			line = TTC_Client.getIn().readLine();
-			System.out.println(line);
-			// game Start!
 
-			if (line.startsWith("GAMESTART")) {
-				try {
-					
-					mainFrame.dispose();
-					gameStart();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		} catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
+	public static void answerCheck(String num) throws IOException {
+		if (answer == Integer.parseInt(num)) {
+			out.println("CORRECT " + name);
+		} else {
+			out.println("INCORRECT " + name);
 		}
-
-		
-	}
-
-	public static void gameStart() throws IOException {
-		
-		System.out.println("enter game start function int TTC_Client");
-		JFrame gameFrame = new JFrame();
-		Container con = gameFrame.getContentPane();
-		gameFrame.setSize(1024, 768);
-		gameFrame.setResizable(false);
-		gameFrame.setVisible(true);
-		
-		String line = in.readLine();
-		System.out.println(line);
-		if (line.startsWith("TEAM")) {
-			line = line.substring(5, line.length());
-			if (line.equalsIgnoreCase("infectee"))
-				con.add(new GameRoomPanel("TTC_Gui_infectee.png"));
-			else
-				con.add(new GameRoomPanel("TTC_Gui_Noninfectee.png"));
-		}
-		receiveProblem();
-}
-	
-	public static void receiveProblem(){
-		/* Problem receiver */
-		String line;	
-		while(true){
-		try {
-			line = in.readLine();
-			System.out.println(line);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-//		if (line.startsWith("PROBLEM")) {
-//			line = line.substring(8,line.length());
-//			System.out.println("line");
-//		} else if (line.startsWith("CHOICE")) {
-//			line = line.substring(8,line.length());
-//		} else if (line.startsWith("ANSWER")) {
-//			answer = line.substring(7, line.length());
-//		} 
-		}
-		
-	}
-
-	public void answerCheck() throws IOException {
-
 	}
 
 	public static void main(String[] args) throws Exception {
