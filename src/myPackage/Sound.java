93abf841ab743913e3c7b1b3fb5bad933cd33d19
Ranie <wlfks369@gmail.com>
2016-12-08 20:52:14 +0900

    more bgm!
    !!

---
+++ src/myPackage/Sound.java
@@ -0,0 +1,26 @@
+package myPackage;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound extends Thread {
+	Sound(String file, boolean Loop) {
+		Clip clip;
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(file)));
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.start();
+			if (Loop)
+				clip.loop(-1);
+			// Loop 값이true면 사운드재생을무한반복시킵니다.
+			// false면 한번만재생시킵니다.
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
